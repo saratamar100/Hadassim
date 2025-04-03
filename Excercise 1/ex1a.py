c1b1c6e0b1184456d097761849f7be788d7a2fc8
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-04-03 15:03:19 +0300

    excercise 1 part 2a

--- Excercise 1/ex1a.py
+++ Excercise 1/ex1a.py
@@ -2,28 +2,30 @@
 M is the length of the file
 The loop goes M/SIZE_SECTION iterations
 Each iteration is SIZE_SECTION to read the section,
-and then the lengrh of frequencies to merge (less than SIZE_SECTION)
+and then the length of frequencies to merge (less than SIZE_SECTION)
 All the loop is (M/SIZE_SECTION)*SIZE_SECTION = M
 Then we have  heapq.nlargest that is cost is O(KlogN) when K is the number of all error codes
 The total is: M + KlogN
 """
 
 import heapq
+
 SIZE_SECTION = 50_000
 
-def find_n_common (file_path, N):
+
+def find_n_common(file_path, N):
     frequency = dict()
-    file = open(file_path,"r")
+    file = open(file_path, "r")
     while True:
         section = file.read(SIZE_SECTION) + file.readline()
         if section == "":
             break
         lines = section.split("\n")
-        if(len(lines[-1])>2):
+        if len(lines[-1]) > 2:
             print("ERROR!!!!!!!!!!!!!!!!!!!!")
         freq = dict()
         for line in lines[:-1]:
-            _ , date, hour, _ , error = line.split()
+            _, date, hour, _, error = line.split()
             if error not in freq:
                 freq[error] = 0
             else:
@@ -34,13 +36,10 @@ def find_n_common (file_path, N):
             else:
                 frequency[f] = freq[f]
 
-    top_n_keys = heapq.nlargest(N, frequency, key=frequency.get) 
-    return list(map(lambda k: [k, frequency[k]], top_n_keys) )
-
-
-
+    top_n_keys = heapq.nlargest(N, frequency, key=frequency.get)
+    return list(map(lambda k: [k, frequency[k]], top_n_keys))
 
 
 n = int(input("Please enter an N: "))
-result = find_n_common("logs.txt",n)
+result = find_n_common("logs.txt", n)
 print(result)
---
+++ Excercise 1/ex1b.py
@@ -0,0 +1,44 @@
+import datetime
+from collections import defaultdict
+import math
+
+
+def read_file(file_path):
+    file = open(file_path, "r")
+    file.readline()
+    all_data = dict()
+    for line in file:
+        try:
+            time, value = line[:-1].split(",")
+            value = float(value)
+            if math.isnan(value):
+                raise Exception("Value foramt exception")
+            date, hour = time.split(" ")
+            DD, MM, YYYY = date.split("/")
+            hh, mm = hour.split(":")
+            if not (len(DD) == len(MM) == len(hh) == len(mm) == 2) or len(YYYY) != 4:
+                raise Exception("Time format exception")
+            date = datetime.datetime(int(YYYY), int(MM), int(DD), int(hh), int(mm))
+            if date not in all_data:
+                all_data[date] = value
+        except:
+            pass
+    return all_data
+
+
+def calculate_avarages(data):
+    dictionary = defaultdict(lambda: [0, 0])
+    for record in data:
+        dictionary[record.replace(minute=0)][0] += data[record]
+        dictionary[record.replace(minute=0)][1] += 1
+
+    file = open("averages.csv", "w")
+    file.write("hour,average value")
+    for time in dictionary:
+        average = dictionary[time][0] / dictionary[time][1]
+        file.write("\n{0},{1}".format(time, average))
+    file.close()
+
+
+standardized_data = read_file("time_series.csv")
+calculate_avarages(standardized_data)
